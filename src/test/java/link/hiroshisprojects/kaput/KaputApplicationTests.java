6ab8c9186c29a8642471147194b6ae03ac7245f7
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-02-08 23:53:43 -0500

    h2 liquibase integration, still debugging

--- src/test/java/link/hiroshisprojects/kaput/KaputApplicationTests.java
+++ src/test/java/link/hiroshisprojects/kaput/KaputApplicationTests.java
@@ -1,13 +1,39 @@
 package link.hiroshisprojects.kaput;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import link.hiroshisprojects.kaput.user.User;
+import link.hiroshisprojects.kaput.user.UserService;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KaputApplicationTests {
 
+	@Autowired
+	UserService userService;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("H2 initialization successful, user found")
+	void dbInit() {
+		List<User> users = new ArrayList<>();
+		Iterable<User> query = userService.getAll();	
+
+		query.forEach(users::add);
+
+		assertEquals(1, users.size());
+
+		User queriedUser = users.get(0);
+
+		assertEquals("test_first", queriedUser.getFirstName());
 	}
 
 }
