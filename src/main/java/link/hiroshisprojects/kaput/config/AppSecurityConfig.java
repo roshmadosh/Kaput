f5af9f3f6a29c3ec0195f70f69ea8f010245f762
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-03-04 16:26:24 -0500

    attempt socket binding for azure app service

--- src/main/java/link/hiroshisprojects/kaput/config/AppSecurityConfig.java
+++ src/main/java/link/hiroshisprojects/kaput/config/AppSecurityConfig.java
@@ -53,7 +53,7 @@ public class AppSecurityConfig {
 				@Override
 				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 					CorsConfiguration config = new CorsConfiguration();
-					config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+					config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
 					config.setAllowedMethods(Collections.singletonList("*"));
 					config.setAllowCredentials(true);
 					config.setAllowedHeaders(Collections.singletonList("*"));
